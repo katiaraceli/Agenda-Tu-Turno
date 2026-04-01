23bf11ea356bfd6ba06e4aa4e8a9cf49805b9742
katiaraceli <racelikatia@gmail.com>
2026-04-01 01:23:53 -0300

    Login exitoso con Google

--- config/env.js
+++ config/env.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT,
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  redirectUri: process.env.GOOGLE_REDIRECT_URI
+    port: process.env.PORT || 3000,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    redirectUri: process.env.GOOGLE_REDIRECT_URI
 };
