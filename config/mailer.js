4d17e12967d47719f9ad66870b012088e5ee085f
katiaraceli <racelikatia@gmail.com>
2026-04-13 10:25:42 -0300

    pass render4

--- config/mailer.js
+++ config/mailer.js
@@ -5,17 +5,21 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // Importante: false para 587
+  port: 465,
+  secure: true, // Siempre true para puerto 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000, // Subimos a 30 segundos
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+  dnsTimeout: 30000,
   tls: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
+    servername: 'smtp.gmail.com' // Esto ayuda a que el certificado coincida
   }
 });
-
 // Verificación de conexión para estar seguros al arrancar
 transporter.verify()
     .then(() => console.log('✅ Gmail vinculado y listo para enviar'))
